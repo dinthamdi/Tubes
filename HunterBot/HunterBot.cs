bb95a3a35d99d6583bdd370b751a19458a934212
dinthamdi <dintani.hamdi@gmail.com>
2026-05-12 21:03:03 +0700

    upload pertama

---
+++ HunterBot/HunterBot.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Drawing;
+using Robocode.TankRoyale.BotApi;
+using Robocode.TankRoyale.BotApi.Events;
+
+public class HunterBot : Bot
+{
+    private int moveDirection = 1;
+    private Random rnd = new Random();
+
+    static void Main(string[] args)
+    {
+        new HunterBot().Start();
+    }
+
+    HunterBot() : base(BotInfo.FromFile("HunterBot.json")) { }
+
+    public override void Run()
+    {
+        BodyColor = Color.Red;
+        GunColor = Color.DarkRed;
+        RadarColor = Color.Orange;
+        BulletColor = Color.Yellow;
+        TracksColor = Color.Black;
+
+        while (IsRunning)
+        {
+            // Radar selalu scan
+            TurnRadarRight(360);
+
+            // Movement agresif tapi tidak barbar
+            Forward(150 * moveDirection);
+
+            // Random turn supaya tidak gampang ditembak
+            TurnRight(rnd.Next(20, 60));
+
+            // Kadang ganti arah
+            if (rnd.Next(0, 100) > 70)
+            {
+                moveDirection *= -1;
+            }
+        }
+    }
+
+    public override void OnScannedBot(ScannedBotEvent e)
+    {
+        // Fire adaptif
+        if (Energy > 70)
+        {
+            Fire(3);
+        }
+        else if (Energy > 40)
+        {
+            Fire(2);
+        }
+        else
+        {
+            Fire(1);
+        }
+
+        // Rush musuh sedikit
+        Forward(80 * moveDirection);
+
+        // Gerakan zigzag setelah scan
+        TurnRight(rnd.Next(15, 45));
+    }
+
+    public override void OnHitWall(HitWallEvent e)
+    {
+        // Anti-wall stuck
+        moveDirection *= -1;
+
+        Back(120);
+        TurnRight(90);
+    }
+
+    public override void OnHitBot(HitBotEvent e)
+    {
+        // Close combat
+        Fire(3);
+
+        // Jangan terlalu lama nempel
+        Back(80);
+
+        TurnRight(45);
+    }
+
+    public override void OnHitByBullet(HitByBulletEvent e)
+    {
+        // Evasive movement
+        moveDirection *= -1;
+
+        TurnLeft(rnd.Next(30, 70));
+
+        Forward(100);
+    }
+}
+
---
+++ SniperBot/SniperBot.cs
@@ -0,0 +1,52 @@
+using System.Drawing;
+using Robocode.TankRoyale.BotApi;
+using Robocode.TankRoyale.BotApi.Events;
+
+public class SniperBot : Bot
+{
+    static void Main(string[] args)
+    {
+        new SniperBot().Start();
+    }
+
+    SniperBot() : base(BotInfo.FromFile("SniperBot.json")) { }
+
+    public override void Run()
+    {
+        BodyColor = Color.Blue;
+        GunColor = Color.DarkBlue;
+        RadarColor = Color.Cyan;
+        BulletColor = Color.LightBlue;
+
+        while (IsRunning)
+        {
+            // Radar muter terus untuk scan musuh
+            TurnRadarRight(360);
+
+            // Movement sniper (jaga jarak)
+            Back(50);
+            TurnRight(30);
+            Forward(80);
+            TurnLeft(30);
+        }
+    }
+
+    public override void OnScannedBot(ScannedBotEvent e)
+    {
+        // Fire ringan agar cepat dan akurat
+        Fire(1);
+    }
+
+    public override void OnHitWall(HitWallEvent e)
+    {
+        Back(100);
+        TurnRight(90);
+    }
+
+    public override void OnHitBot(HitBotEvent e)
+    {
+        Back(50);
+        Fire(2);
+    }
+}
+
